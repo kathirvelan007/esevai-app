ebea25d9da37f2aaa7070a5d8b9e61c8ca4d2b80
Kathirvelan <kathirvelan@Kathirvelans-MacBook-Air.local>
2025-11-08 12:52:55 +0530

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1 className="logo">Kathirvelan</h1>
+      <h1 className="logo">Kathirvelan Portfolio</h1>
       <nav className="nav">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
